2a3f0fb48dbff3b40d311f603dbe023daaf49a9f
daima2018 <1138393283@qq.com>
2021-01-21 11:58:10 +0800

    tj2

--- src/main/java/org/ymx/App.java
+++ src/main/java/org/ymx/App.java
@@ -1,5 +1,9 @@
 package org.ymx;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,15 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SimpleDateFormat simpleDateFormat2 = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss-07:00");
+//        Date date=new Date();
+//        String dateString=simpleDateFormat2.format(date);
+        try {
+            Date parse = simpleDateFormat2.parse("2020-09-06T20:44:27-07:00");
+            System.out.println(parse);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
     }
 }
