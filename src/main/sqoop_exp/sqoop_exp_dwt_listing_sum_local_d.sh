bd49bea8dc02d18467c8cae8aa19f18c526e072e
daima2018 <1138393283@qq.com>
2021-01-21 20:10:38 +0800

    tj

--- src/main/sqoop_exp/sqoop_exp_dwt_listing_sum_local_d.sh
+++ src/main/sqoop_exp/sqoop_exp_dwt_listing_sum_local_d.sh
@@ -21,17 +21,23 @@ tmp_dbname=ymx_tmp
 hive_tbname=dwt_listing_sum_local_d
 tmp_tbname=dwt_listing_sum_local_d   #不需要建表,只要路径就行
 
-target_dir=${HDFS_BASE_DIR}/${tmp_dbname}.db/${tmp_tbname}
+target_dir=${HDFS_BASE_DIR}/${tmp_dbname}.db/${tmp_tbname}/company_code=${company_code}
 
 echo "--target_dir:${target_dir}"
 
-db_connect=`eval echo  '$'"${company_code}_db_connect"`
-db_username=`eval echo '$'"${company_code}_db_username"`
-db_password=`eval echo '$'"${company_code}_db_password"`
+# db_connect=`eval echo  '$'"${company_code}_db_connect"`
+# db_username=`eval echo '$'"${company_code}_db_username"`
+# db_password=`eval echo '$'"${company_code}_db_password"`
+
+db_connect=jdbc:mysql://hdp101:3306/ymx
+db_username=root
+db_password=12345678
 
 echo "--connect:${db_connect}"
 echo "--db_username:${db_username}"
 echo "--db_password:${db_password}"
+
+columns=user_account,site,seller_sku,asin,qty,summary_date,sale_amount,sale_amount_usd,sale_amount_eur,sale_amount_gbp,sale_amount_jpy,sale_amount_original,created_time,updated_time,sale_order_num,refund_amount,refund_money,refund_money_usd,refund_money_eur,refund_money_gbp,refund_money_jpy,refund_money_local,key1,return_amount,asin_type,ad_qty,ad_sale_amount,ad_sale_amount_usd,ad_sale_amount_eur,ad_sale_amount_gbp,ad_sale_amount_jpy,ad_sale_order_num,ad_sale_amount_original,cost,cost_local,cost_usd,cost_eur,cost_gbp,cost_jpy,clicks,impressions,sessions,page_views,buy_box_percentage,session_percentage,page_views_percentage
 
 hive -e "
 insert overwrite direcotry '${target_dir}'
@@ -41,6 +47,7 @@ select
     ,seller_sku
     ,asin
     ,qty
+    ,stat_date as summary_date
     ,sale_amount
     ,sale_amount_usd
     ,sale_amount_eur
@@ -100,9 +107,10 @@ if [ $DISK_SPACE -gt 0 ];then
     --connect ${db_connect} \
     --username ${db_username} \
     --password ${db_password} \
-    --table listing_sum_local_d              \
+    --table ec_amazon_listing_extend_summary_local     \
     --num-mappers 1                          \
     --export-dir ${target_dir}   \
+    --columns ${columns}     \
     --input-fields-terminated-by "\t"          \
     --input-null-string '\\N'                    \
     --input-null-non-string '\\N'               
