d1c8caf792c6ffe6cefb57f4441329cdf00eeb69
daima2018 <1138393283@qq.com>
2021-02-03 15:53:34 +0800

    tj

---
+++ src/main/shell/reload_account.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#############################################################
+#名称   解析账号脚本
+#############################################################
+
+getpassword(){
+    arg=$1
+    result=`php -r "echo openssl_decrypt($arg, 'des-ecb', 'AMAZON');"`
+    echo $result
+}
+
+src_name=/home/ecm/ymx/conf/account.txt
+tmp_name=/home/ecm/ymx/conf/tmp-conf.sh
+target_name=/home/ecm/ymx/conf/sqoop-job-conf.sh
+
+#清空文件内容
+echo "" > $tmp_name
+echo "\#\!/bin/bash" >> $tmp_name
+echo "HDFS_BASE_DIR=/user/hive/warehouse/" >> $tmp_name
+
+for line in `cat $src_name`
+do
+    a=`echo $line|awk -F "#=#" '{print $1}'`
+    b=`echo $line|awk -F "#=#" '{print $2}'`
+    if [ "${a:0-8:8}" = "password" ] 
+    then b="\""`getpassword $b`"\""
+        echo $a=$b >> $tmp_name
+        echo "" >> $tmp_name
+    else echo $a=$b >> $tmp_name   
+    fi    
+done
+
+#内容覆盖
+cp $tmp_name $target_name
+xsync $target_name
